0e81c41885a53b10a9e6765d9503844256da4fd8
croftwell <90102689+croftwell@users.noreply.github.com>
2023-06-04 14:18:08 +0300

    son

---
+++ Lms/README.md
@@ -0,0 +1,4 @@
+# Library Management System
+Internet Based Programming Project
+Mehmet Ali Çavuşlu - 2010206044 Computer Engineering
+Library Management System is a web application that exactly build for college and university base library management system. It built in simple raw php and simple database design.
---
+++ Lms/Source/librarian/user/contactus.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Library Management System</title>
+	<link rel="icon" type="image/png" href="dist/img/mm.png">
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="dist/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="dist/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="dist/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="dist/css/animate.css">
+	<link rel="stylesheet" href="dist/css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Exo+2:400,500,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-3">
+					<div class="logo">
+						<img src="dist/img/1.png" alt="logo">
+					</div>
+				</div>
+				<div class="col-9">
+					<div class="header-right">
+						<ul>
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin"></i></a></li>
+							<li><a href="index.php">Home</a></li>
+						</ul>		
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contactinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="left">
+						<h3>Send Us a Message</h3>
+						<form action="">
+							<input type="text" placeholder="Your Name">
+							<input type="email" placeholder="Your E-mail">
+							<input type="digit" placeholder="Phone Number">
+							<textarea placeholder="Your Message" id="" cols="60" rows="5"></textarea>
+							<input type="submit" value="SEND A MESSAGE">
+						</form>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="right">
+						<h3>Contact Info</h3>
+						<p>Hiçbirimiz hepimiz kadar iyi olamayız.</p>
+
+						<div class="box">
+							<p><i class="fas fa-home"></i><span> Ankara</span></p>
+							<p><i class="far fa-envelope"></i><span> cavuslumehmet@icloud.com</span></p>
+							<p><i class="fas fa-phone"></i><span> 0536 681 06 75</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="map-responsive mapw">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12006.486296569947!2d32.6574561!3d41.2082232!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4083535a4c97b665%3A0xbeb2785ee29c578!2zS2FyYWLDvGsgw5xuaXZlcnNpdGVzaSBLYW1pbCBHw7xsZcOnIEvDvHTDvHBoYW5lc2k!5e0!3m2!1str!2str!4v1684855125878!5m2!1str!2str" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</div>
+    <div class="footer text-center">
+        <p style="margin: 20px; font-weight: bold;"><span>copyright &copy; Mehmet Ali Çavuşlu</span></p>
+    </div>
+
+	<script src="dist/js/jquery-2.2.4.min.js"></script>
+	<script src="dist/js/bootstrap.min.js"></script>
+	<script src="dist/js/owl.carousel.min.js"></script>
+	<script src="dist/js/owl.animate.js"></script>
+	<script src="dist/js/superfish.js"></script>
+	<script src="dist/js/waypoints.min.js"></script>
+	<script src="dist/js/jquery.counterup.min.js"></script>
+	<script src="dist/js/custom.js"></script>
+</body>
+</html>
